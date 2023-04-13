c95e6dea04fd5f5dd12ef1fbb073b2b436a9400d
Pinak Paliwal <37413746+pythoncrazy@users.noreply.github.com>
2023-04-13 01:49:08 -0700

    Started making the leaderboard code

--- make_leaderboard.py
+++ make_leaderboard.py
@@ -6,7 +6,7 @@ import csv #I need this in order to add data to the tsv, tab separated file.
 with open('./lichess.token') as f:
     token = f.read()
 session = berserk.TokenSession(token)
-client = berserk.client.users(session)
+client = berserk.Client(session)
 
 name_of_bot_accounts = list(line.strip() for line in open('bullet_bot.names')) # Maybe not the most efficient way of doing this, but it works and I want to do it this way.
 
@@ -15,5 +15,5 @@ with open('./Bullet.csv','w',newline='') as f_output:         # https://stackove
     bot_information = []
     for name_of_bot in name_of_bot_accounts: # iterate over all of the bot accounts
         name = name_of_bot
-        max_rating = 
+        max_rating = client.users.get_performance_statistics(name,"bullet") #for some reason, the pypi berserk package does not have this function. I have manually added it from their github repo: https://github.com/rhgrant10/berserk/blob/master/berserk/clients.py#L329
         tsv_output.writerow(data)
