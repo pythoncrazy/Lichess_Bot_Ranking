b57b67d3a1207f5f3d5bd89f8d5c3c2439670ab0
Pinak Paliwal <37413746+pythoncrazy@users.noreply.github.com>
2023-04-18 13:01:26 -0700

    Made blitz max

--- app.py
+++ app.py
@@ -12,17 +12,24 @@ app = Flask(__name__)
 #TODO: expand to all rating systems, as well as current and maximum rating
 @app.route('/')
 @app.route('/bullet_curr')
-def current_rating():
+def bullet_current_rating():
 	
 	# converting csv to html
-	data = pd.read_csv('bullet_sorted_by_current_rating.csv',header=0)
+	data = pd.read_csv('bullet/bullet_sorted_by_current_rating.csv',header=0)
 	return render_template('Current.html', tables=[data.to_html()], titles=[''])
 
 @app.route('/bullet_max')
-def max_rating():
+def bullet_max_rating():
 	
 	# converting csv to html
-	data = pd.read_csv('bullet_sorted_by_max_rating.csv',header=0)
+	data = pd.read_csv('bullet/bullet_sorted_by_max_rating.csv',header=0)
+	return render_template('Maximum.html', tables=[data.to_html()], titles=[''])
+
+@app.route('/blitz_max')
+def blitz_max_rating():
+	
+	# converting csv to html
+	data = pd.read_csv('blitz/blitz_sorted_by_max_rating.csv',header=0)
 	return render_template('Maximum.html', tables=[data.to_html()], titles=[''])
 
 if __name__ == "__main__":
