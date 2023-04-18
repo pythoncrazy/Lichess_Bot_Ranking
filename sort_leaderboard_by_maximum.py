aae2f106724460e43c48af34f29266533c275b38
Pinak Paliwal <37413746+pythoncrazy@users.noreply.github.com>
2023-04-18 12:58:06 -0700

    Modular Code is so much fun, I got all of the variants made!
    I love modular code so much please help me. I LOVE OOP I LOVE OOP I LOVE OOP

--- sort_leaderboard_by_maximum.py
+++ sort_leaderboard_by_maximum.py
@@ -1,8 +1,13 @@
 import pandas as pd #pandas is too good for csv files
   
-# making data frame from csv file
-data = pd.read_csv("bullet.csv")
-data.sort_values("Maximum Rating", axis=0, ascending=False,
-                 inplace=True, na_position='first')
-print(data)
-data.to_csv('bullet_sorted_by_max_rating.csv', index=False)
+
+variants = ["bullet","blitz","rapid"]
+def sort_by_max_csv(variant):
+    # making data frame from csv file
+    data = pd.read_csv(variant+"/"+variant+".csv")
+    data.sort_values("Maximum Rating", axis=0, ascending=False,
+                    inplace=True, na_position='first')
+    print(data)
+    data.to_csv(variant+"/"+variant+'_sorted_by_max_rating.csv', index=False)
+for variant in variants:
+    sort_by_max_csv(variant)
