9b4a876115acd7da2465cf21ca096da4235ff0fc
Pinak Paliwal <37413746+pythoncrazy@users.noreply.github.com>
2023-04-18 12:27:32 -0700

    Made the code more modular, which is always good
    I love modular and object oriented programming. I LOVE OOP I LOVE OOP I LOVE OOP I LOVE OOP

--- gather_ranks.py
+++ gather_ranks.py
@@ -8,23 +8,28 @@ with open('./lichess.token') as f:
 session = berserk.TokenSession(token)
 client = berserk.clients.Bots(session)
 
-#get the list of all 2000000000000 bots accounts online
-# I do realize that there could one day be more than 200000000000 bot accounts online, but it is very unlikely
-online_bots = client.get_online(nb=20000000000)
+def create_bot_txt(variant): 
 
-name_of_bot_accounts = set(line.strip() for line in open('bullet_bot.names')) # Read in all of the previous bot accounts into a set. This allows for any bots I manually add to be kept, but not have duplicates.
+    #get the list of all 2000000000000 bots accounts online
+    # I do realize that there could one day be more than 200000000000 bot accounts online, but it is very unlikely
+    online_bots = client.get_online(nb=20000000000)
 
-for i in online_bots:
-    name_of_bot = i['id']
-    #print(name_of_bot,i.keys())
-    if (i["perfs"]["bullet"]["rd"]<100): # filter out the bot accounts that are inactive in bullet. I choose 100 because I feel that most active bots can reach sub 100 rd. I did not choose 75, since some bots don't always get to play other bots at their rating. I feel very strongly on this, but will most likely change it at some future point in time.
-        try:
-            if(i["tosViolation"]): #exclude bots that have a TOS Violation, since they shouldn't be on the list anyways
-                continue
-        except:
-            name_of_bot_accounts.add(name_of_bot)
-name_of_bot_accounts = list(name_of_bot_accounts)
-name_of_bot_accounts.sort() #I want it sorted because it looks better. It has a minimal impact on performance, but it feels better and cleaner. Just like washing your hands. 
-with open('bullet_bot.names','w') as f:
-    for i in name_of_bot_accounts:
-        f.write(i+"\n")
+    name_of_bot_accounts = set(line.strip() for line in open(variant+"/"+variant+'_bot.names')) # Read in all of the previous bot accounts into a set. This allows for any bots I manually add to be kept, but not have duplicates.
+
+    for i in online_bots:
+        name_of_bot = i['id']
+        #print(name_of_bot,i.keys())
+        if (i["perfs"][variant]["rd"]<100): # filter out the bot accounts that are inactive in bullet. I choose 100 because I feel that most active bots can reach sub 100 rd. I did not choose 75, since some bots don't always get to play other bots at their rating. I feel very strongly on this, but will most likely change it at some future point in time.
+            try:
+                if(i["tosViolation"]): #exclude bots that have a TOS Violation, since they shouldn't be on the list anyways
+                    continue
+            except:
+                name_of_bot_accounts.add(name_of_bot)
+    name_of_bot_accounts = list(name_of_bot_accounts)
+    name_of_bot_accounts.sort() #I want it sorted because it looks better. It has a minimal impact on performance, but it feels better and cleaner. Just like washing your hands. 
+    with open(variant+"/"+variant+'_bot.names','w') as f:
+        for i in name_of_bot_accounts:
+            f.write(i+"\n")
+create_bot_txt("bullet")
+create_bot_txt("blitz")
+create_bot_txt("rapid")
