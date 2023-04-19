7f1888efc06da167b3aa5476c65a300dadd4b0ae
Pinak Paliwal <37413746+pythoncrazy@users.noreply.github.com>
2023-04-18 20:43:43 -0700

    Added some more variants, as well as a general information page.

--- app.py
+++ app.py
@@ -10,8 +10,13 @@ app = Flask(__name__)
 
 # route to the bullet current ratings
 #TODO: expand to all rating systems, as well as current and maximum rating
+#DONE :)
+#TODO: make homepage
 @app.route('/')
-@app.route('/bullet_curr')
+def welcome_page():
+    return render_template('Welcome.html')
+
+@app.route('/bullet_current')
 def bullet_current_rating():
 	
 	# converting csv to html
@@ -25,12 +30,34 @@ def bullet_max_rating():
 	data = pd.read_csv('bullet/bullet_sorted_by_max_rating.csv',header=0)
 	return render_template('Maximum.html', tables=[data.to_html()], titles=[''])
 
+@app.route('/blitz_current')
+def blitz_current_rating():
+	
+	# converting csv to html
+	data = pd.read_csv('blitz/blitz_sorted_by_current_rating.csv',header=0)
+	return render_template('Maximum.html', tables=[data.to_html()], titles=[''])
+
 @app.route('/blitz_max')
 def blitz_max_rating():
 	
 	# converting csv to html
 	data = pd.read_csv('blitz/blitz_sorted_by_max_rating.csv',header=0)
 	return render_template('Maximum.html', tables=[data.to_html()], titles=[''])
+
+@app.route('/rapid_current')
+def rapid_current_rating():
+	
+	# converting csv to html
+	data = pd.read_csv('rapid/rapid_sorted_by_current_rating.csv',header=0)
+	return render_template('Maximum.html', tables=[data.to_html()], titles=[''])
+
+@app.route('/rapid_max')
+def rapid_max_rating():
+	
+	# converting csv to html
+	data = pd.read_csv('rapid/rapid_sorted_by_max_rating.csv',header=0)
+	return render_template('Maximum.html', tables=[data.to_html()], titles=[''])
+
 
 if __name__ == "__main__":
 	app.run(host="localhost", port=int("5000"))
