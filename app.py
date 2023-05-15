0016b71f8d668d228b351a38d682892bd5f0c076
Pinak Paliwal <37413746+pythoncrazy@users.noreply.github.com>
2023-05-15 15:06:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def rapid_max_rating():
 	data = pd.read_csv('rapid/rapid_sorted_by_max_rating.csv',header=0)
 	return render_template('Maximum.html', tables=[data.to_html()], titles=[''])
 
-app.run(host="localhost", port=int("5000"))
+#app.run(host="localhost", port=int("5000")) For Hosting on Render
